1c6122870e0d8428f540cd490568db0dd6cc9012
NISHANIRUBERT <51689306+NISHANIRUBERT@users.noreply.github.com>
2019-06-11 14:10:06 +0530

    Create kthlargest.c

---
+++ kthlargest.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int size,*arr,k,index,index2;
+	scanf("%d %d",&size,&k);
+	arr=(int*)malloc(size*sizeof(int));
+	for(index=1;index<=size;index++)
+	scanf("%d ",&arr[index]);
+	for(index=1;index<=size;index++)
+	{
+		for(index2=index+1;index2<=size;index2++)
+		{
+			if(arr[index]<arr[index2])
+			{
+				int temp=arr[index];
+				arr[index]=arr[index2];
+				arr[index2]=temp;
+			}
+		}
+	}
+	printf("%d ",arr[k]);
+	
+	return 0;
+}
