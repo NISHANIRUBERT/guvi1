bb4de75274eab777fd034005dda422a1bf2602e6
NISHANIRUBERT <51689306+NISHANIRUBERT@users.noreply.github.com>
2019-06-12 18:18:50 +0530

    Update vowelorconsonant.c

--- vowelorconsonant.c
+++ vowelorconsonant.c
@@ -4,7 +4,7 @@ int main(void) {
 	// your code goes here
 	char ch;
 	scanf("%c",&ch);
-	if((ch>=95&&ch<=90)||(ch>=97&&ch<=122))
+	if((ch>=65&&ch<=90)||(ch>=97&&ch<=122))
 	{
 	if(strchr("aeiouAEIOU",ch)!=0)
 	printf("Vowel");
