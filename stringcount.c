92985172cf4b9e4ee3c379b0f6bc132f55454e8d
NISHANIRUBERT <51689306+NISHANIRUBERT@users.noreply.github.com>
2019-06-12 18:51:00 +0530

    Update stringcount.c

--- stringcount.c
+++ stringcount.c
@@ -4,9 +4,11 @@ int main(void) {
 	// your code goes here
 	int count,times;
 	scanf("%d",&count);
+	if(count>0)
+	{
 	for(times=1;times<count;times++)
 	printf("Hello\n");
 	printf("Hello");
-	
+	}
 	return 0;
 }
